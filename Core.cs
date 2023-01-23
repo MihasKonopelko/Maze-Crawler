866f702c7a1a0769ab3d8a527bec1817633896e1
Mihass Konopelko <mihapelko93@gmail.com>
2023-01-23 14:38:31 +0000

    b1 - Add Myra for the purposes of UI and menus.

--- Core.cs
+++ Core.cs
@@ -2,10 +2,16 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using Myra;
+using Myra.Graphics2D.UI;
+
+
+
 namespace Maze_Crawler
 {
     public class Core : Game
     {
+        private Desktop _desktop;
         private GraphicsDeviceManager _graphicsDevice;
         private SpriteBatch _spriteBatch;
 
@@ -24,6 +30,68 @@ namespace Maze_Crawler
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            MyraEnvironment.Game = this;
+
+            var grid = new Grid
+            {
+                RowSpacing = 8,
+                ColumnSpacing = 8
+            };
+
+            grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
+            grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
+            grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+            grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+
+            var helloWorld = new Label
+            {
+                Id = "label",
+                Text = "Hello, World!"
+            };
+            grid.Widgets.Add(helloWorld);
+
+            // ComboBox
+            var combo = new ComboBox
+            {
+                GridColumn = 1,
+                GridRow = 0
+            };
+
+            combo.Items.Add(new ListItem("Red", Color.Red));
+            combo.Items.Add(new ListItem("Green", Color.Green));
+            combo.Items.Add(new ListItem("Blue", Color.Blue));
+            grid.Widgets.Add(combo);
+
+            // Button
+            var button = new TextButton
+            {
+                GridColumn = 0,
+                GridRow = 1,
+                Text = "Show"
+            };
+
+            button.Click += (s, a) =>
+            {
+                var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
+                messageBox.ShowModal(_desktop);
+            };
+
+            grid.Widgets.Add(button);
+
+            // Spin button
+            var spinButton = new SpinButton
+            {
+                GridColumn = 1,
+                GridRow = 1,
+                Width = 100,
+                Nullable = true
+            };
+            grid.Widgets.Add(spinButton);
+
+            // Add it to the desktop
+            _desktop = new Desktop();
+            _desktop.Root = grid;
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +105,7 @@ namespace Maze_Crawler
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _desktop.Render();
             base.Draw(gameTime);
         }
     }
